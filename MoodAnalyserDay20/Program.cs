fc861903a2624d89f7373fb295e7420ca6d7a84a
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-01-14 19:08:47 +0530

    Added Welcome to Mood Analyser Programs

---
+++ MoodAnalyserDay20/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserDay20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser Programs");
+            Console.ReadLine();
+        }
+    }
+}
